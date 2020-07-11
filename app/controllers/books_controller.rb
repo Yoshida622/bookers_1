aae7555eb931a2f9ca75ee40be62a9b312774f7f
vagrant <vagrant@localhost.localdomain>
2020-07-11 15:59:42 +0000

    [Start]Rspec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,51 @@
 class BooksController < ApplicationController
+  def root
+  end
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+       redirect_to book_path(book.id), notice: 'successfully'
+    else
+       @books = Book.all
+       @book = Book.new
+       flash.now[:alert] = "error"
+       render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+       redirect_to book_path(book.id), notice: 'successfully'
+    else
+       @book = Book.find(params[:id])
+       flash.now[:alert] = "error"
+       render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
